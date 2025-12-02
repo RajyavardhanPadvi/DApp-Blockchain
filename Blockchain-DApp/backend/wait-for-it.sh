e31d7d75cdcefef0b01092dc5befc923a19e9cbc
RajyavardhanPadvi <rpadvi2003@gmail.com>
2025-12-02 19:28:31 +0530

    Add files via upload

---
+++ Blockchain-DApp/backend/wait-for-it.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+host="$1"
+port="$2"
+shift 2
+cmd="$@"
+
+echo "Waiting for $host:$port..."
+
+while ! nc -z "$host" "$port" 2>/dev/null; do
+  sleep 1
+done
+
+echo "$host:$port is available"
+sleep 5
+
+exec $cmd
+
